c722b701d16ce339b84b4241fc37c56dce670b2f
IOgnen <ogneniloski@gmail.com>
2023-09-08 13:26:58 +0200

    frontend Update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,11 +11,13 @@
   </header>
   <main>
     <RobotBuilder />
+    <Search />
   </main>
 </template>
 <script>
-import RobotBuilder from './build/RobotBuilder.vue';
 // import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
+// import Search from './search/Search.vue';
 
 export default {
   name: 'App',
@@ -24,39 +26,40 @@ export default {
   },
 };
 </script>
-
-<style>
-body {
-  background: linear-gradient(to bottom, #555, #999);
-  background-attachment: fixed;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-main {
-  margin: 0 auto;
-  padding: 30px;
-  background-color: white;
-  width: 1024px;
-  min-height: 300px;
-}
-header {
-  background-color: #999;
-  width: 1084px;
-  margin: 0 auto;
-}
-ul {
-  padding: 3px;
-  display: flex;
-}
-.nav-item {
-  display: inline-block;
-  padding: 5px 10px;
-  font-size: 22px;
-  border-right: 1px solid #bbb;
-}
-.logo {
-  vertical-align: middle;
-  height: 30px;
-}
+<style >
+  body {
+    background: linear-gradient(to bottom, #555, #999);
+    background-attachment: fixed;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  }
+</style>
+<style scoped>
+  main {
+    margin: 0 auto;
+    padding: 30px;
+    background-color: white;
+    width: 1024px;
+    min-height: 300px;
+  }
+  header {
+    background-color: #999;
+    width: 1084px;
+    margin: 0 auto;
+  }
+  ul {
+    padding: 3px;
+    display: flex;
+  }
+  .nav-item {
+    display: inline-block;
+    padding: 5px 10px;
+    font-size: 22px;
+    border-right: 1px solid #bbb;
+  }
+  .logo {
+    vertical-align: middle;
+    height: 30px;
+  }
 </style>
